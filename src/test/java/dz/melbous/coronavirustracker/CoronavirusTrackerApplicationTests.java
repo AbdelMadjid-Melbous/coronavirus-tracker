3029527122169bd9dae21fbd9501156988a98977
AbdelmadjidMelbous <ea_melbous@esi.dz>
2020-06-19 16:08:15 +0100

    initial commit

---
+++ src/test/java/dz/melbous/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.melbous.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
